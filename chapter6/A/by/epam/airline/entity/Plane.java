0a49239125b06dd8b3efa5480bc420d154efa1bd
Egor <148298503+AwesomeWhiteWolf@users.noreply.github.com>
2026-02-28 11:14:58 +0800

    Create Plane.java

---
+++ chapter6/A/by/epam/airline/entity/Plane.java
@@ -0,0 +1,32 @@
+package by.epam.airline.entity;
+
+import java.util.StringJoiner;
+
+public abstract class Plane {
+    private String model;
+    private String id;
+    private boolean isReadyForFlight;
+
+    public Plane(String model, String id) {
+        this.model = model;
+        this.id = id;
+    }
+
+    // Методы для получения и изменения информации
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+    
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public boolean isReadyForFlight() { return isReadyForFlight; }
+    public void setReadyForFlight(boolean ready) { isReadyForFlight = ready; }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", this.getClass().getSimpleName() + "[", "]")
+                .add("id='" + id + "'")
+                .add("model='" + model + "'")
+                .toString();
+    }
+}
