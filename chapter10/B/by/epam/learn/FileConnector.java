68b746d0767e8bcf900098e213884efc72f2dea2
Egor <148298503+AwesomeWhiteWolf@users.noreply.github.com>
2026-04-07 07:16:27 +0800

    Create FileConnector.java

---
+++ chapter10/B/by/epam/learn/FileConnector.java
@@ -0,0 +1,20 @@
+package by.epam.learn.FileConnector;
+
+import java.io.*;
+import java.util.*;
+
+import java.io.*;
+
+class FileConnector {
+    public static void saveAirline(Airline airline, String filename) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(airline);
+        }
+    }
+
+    public static Airline loadAirline(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (Airline) ois.readObject();
+        }
+    }
+}
